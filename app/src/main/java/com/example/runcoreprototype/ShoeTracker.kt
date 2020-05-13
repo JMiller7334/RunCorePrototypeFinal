3060298c79d3877ff111e7dd331d09f708654b51
JMiller7334 <42582030+JMiller7334@users.noreply.github.com>
2020-05-13 13:57:52 -0500

    Coding done 3/4

--- app/src/main/java/com/example/runcoreprototype/ShoeTracker.kt
+++ app/src/main/java/com/example/runcoreprototype/ShoeTracker.kt
@@ -1,12 +1,37 @@
 package com.example.runcoreprototype
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class ShoeTracker : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shoe_tracker)
+
+        val buttonBack = findViewById<Button>(R.id.idButtonBack1)
+        buttonBack.setOnClickListener {
+            val screenWorkout = Intent(this, WorkoutScreen::class.java)
+            startActivity(screenWorkout)
+        }
+
+        val displayShoeName = findViewById<TextView>(R.id.idDisplayShoeName)
+        val displayShoeMileage = findViewById<TextView>(R.id.idDisplayShoeMileage)
+
+        val enterShoeName = findViewById<TextView>(R.id.idEnterShoeName)
+        val enterShoeMileage = findViewById<TextView>(R.id.idEnterShoeMileage)
+        val buttonTrackerSave = findViewById<TextView>(R.id.idButtonShoeSave)
+        buttonTrackerSave.setOnClickListener {
+            val newName = enterShoeName.text.toString()
+            val newMileage = enterShoeMileage.text.toString()
+
+            displayShoeName.text = newName
+            displayShoeMileage.text = newMileage
+        }
+
+
     }
 }
