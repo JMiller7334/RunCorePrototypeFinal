9e314843412748075d8685b99981a9363ae4501a
JMiller7334 <42582030+JMiller7334@users.noreply.github.com>
2020-05-13 11:28:58 -0500

    Interface Mostly done

---
+++ app/src/main/java/com/example/runcoreprototype/ShoeTracker.kt
@@ -0,0 +1,12 @@
+package com.example.runcoreprototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShoeTracker : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shoe_tracker)
+    }
+}
--- app/src/main/java/com/example/runcoreprototype/WorkoutScreen.kt
+++ app/src/main/java/com/example/runcoreprototype/WorkoutScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class WorkoutScreen : AppCompatActivity() {
 
@@ -15,6 +16,17 @@ class WorkoutScreen : AppCompatActivity() {
         buttonLogout.setOnClickListener {
             val screenLogin = Intent(this,MainActivity::class.java)
             startActivity(screenLogin) // connection
+        }
+
+        val buttonTracker = findViewById<Button>(R.id.idButtonShoeTracker)
+        buttonTracker.setOnClickListener {
+            val screenTracker = Intent(this, ShoeTracker::class.java)
+            startActivity(screenTracker)
+        }
+
+        val buttonStartWorkout = findViewById<Button>(R.id.idButtonStartWorkout)
+        buttonStartWorkout.setOnClickListener {
+            Toast.makeText(applicationContext, "Workout will now start...", Toast.LENGTH_SHORT).show()
         }
     }
 }
